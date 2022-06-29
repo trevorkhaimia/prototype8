ce2b482297d8cb99e3f11baac21d94039d3b9f74
trevorkhaimia <trevorkhaimia@gmail.com>
2022-06-29 22:02:15 +0200

    cant make it work

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/gocolly/colly"
+)
+
+func scraper() {
+	filename := "data.csv"
+	file, err := os.Create(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+	c := colly.NewCollector(
+		colly.AllowedDomains("internshala.com"),
+	)
+	c.OnHTML(".internship_meta", func(e *colly.HTMLElement) {
+		writer.Write([]string{
+			e.ChildText("a"),
+			e.ChildText("span"),
+		})
+	})
+
+	//loops 200 pages
+	for i := 0; i < 20; i++ {
+		fmt.Println("web scaping page", i)
+		c.Visit("https://internshala.com/internships/page-" + strconv.Itoa(i))
+		break
+	}
+	log.Printf("it works\n")
+	log.Panic(c)
+}
+func main() {
+	scraper()
+}
